c63444d9ea01b038b879c7910fc35d54dd8a4faa
scardummichele <scarpa.michele.90@gmail.com>
2024-11-11 05:34:58 +0100

    test next

---
+++ pokedex-next/src/app/pokedexlist/page.tsx
@@ -0,0 +1,13 @@
+import Link from "next/link";
+
+export default function PokedexList() {
+
+    return (
+        <div>
+
+            <h1>PokedexList ciao</h1>
+            <h2><Link href="/">Home</Link></h2>
+        </div>
+
+    )
+}
