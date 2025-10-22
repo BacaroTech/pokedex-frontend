b79441c6875ee89506a1de127ae839a00ac77eea
JollyMick <scarpa.michele.90@gmail.com>
2025-10-22 11:58:53 +0200

    rename app -> pokedex next project

--- frameworks/pokedex-next-v1/app/page.tsx
+++ frameworks/pokedex-next-v1/app/page.tsx
@@ -3,7 +3,7 @@ import Card from '@/components/Card';
 export default function Home() {
   return (
     <>
-      <Card href="/app" title="Pokedex" description="Go To Pokedex app" />
+      <Card href="/pokedex" title="Pokedex" description="Go To Pokedex app" />
       <Card href="/sandbox" title="Sandbox" description="Go To Sandbox tes" />
 
     </>
